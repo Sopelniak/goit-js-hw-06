2277fb9a0a64ce0dbd0ad6cc037193f71a892ca5
Sopelniak <106702004+Sopelniak@users.noreply.github.com>
2022-08-27 00:04:32 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+const etemEls = [];
+
+ingredients.forEach(function (ingredient) {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = ingredient;
+  itemEl.classList.add("item");
+  etemEls.push(itemEl);
+});
+
+ingredientsList.append(...etemEls);
+console.log(ingredientsList);
