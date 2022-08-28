363bd0ec6d4eacb8cb461c94d71a903baa1bc3e0
Sopelniak <106702004+Sopelniak@users.noreply.github.com>
2022-08-29 01:19:48 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const btnEl = document.querySelector(".change-color");
+const textEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+btnEl.addEventListener("click", onBtnClick);
+
+function onBtnClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+
+  textEl.textContent = getRandomHexColor();
 }
