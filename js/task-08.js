c9400f815a4c90c39f0a578165cab2bacf613ba1
Sopelniak <106702004+Sopelniak@users.noreply.github.com>
2022-08-29 00:40:17 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  const user = {};
+  user[email.name] = email.value;
+  user[password.name] = password.value;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log(user);
+
+  event.currentTarget.reset();
+}
