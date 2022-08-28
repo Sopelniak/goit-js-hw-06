bb66028bee9d6ad97d4000c45f3f489926f81695
Sopelniak <106702004+Sopelniak@users.noreply.github.com>
2022-08-28 22:50:41 +0300

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const inputEl = document.querySelector("#font-size-control");
+const spanEl = document.querySelector("#text");
+
+inputEl.addEventListener("input", onInputChange);
+
+function onInputChange(event) {
+  spanEl.style.fontSize = event.currentTarget.value + "px";
+  console.log(event.currentTarget.value);
+}
